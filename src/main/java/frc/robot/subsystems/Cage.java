73f730692d877d139368de4488778bfbc14e877a
SCPhillips06 <128173664+SCPhillips06@users.noreply.github.com>
2025-01-10 18:27:00 -0500

    added the cage sub system.

---
+++ src/main/java/frc/robot/subsystems/Cage.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import frc.robot.enums.CageState;
+import frc.robot.statemachine.StateBasedSubsystem;
+
+public class Cage extends StateBasedSubsystem<CageState> { 
+    public Cage (){ 
+
+    }
+    
+    @Override
+    public void periodic (){
+
+    }
+
+}
