42537c861deb9e44d7b58fd9ddfb7f582461e93c
Kacper <katomczyknyc@gmail.com>
2025-01-11 14:41:25 -0500

    Added KitbotRoller subsystem FOR REAL

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Cage;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.KitbotRoller;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -22,13 +23,17 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final Cage _cage = new Cage();
+  private final Cage _cage;
+  private final KitbotRoller _roller;
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    _cage = new Cage();
+    _roller = new KitbotRoller();
+
     // Configure the trigger bindings
     configureBindings();
   }
---
+++ src/main/java/frc/robot/enums/RollerState.java
@@ -0,0 +1,11 @@
+package frc.robot.enums;
+
+import frc.robot.statemachine.IState;
+
+public enum RollerState implements IState {
+
+    STOP,
+
+    ROLL
+    
+}
