07331491531bcaa08f7d48d670bd7f7b602d026b
Kacper <katomczyknyc@gmail.com>
2025-01-11 14:38:44 -0500

    WIP added kitbotroller class

---
+++ src/main/java/frc/robot/subsystems/KitbotRoller.java
@@ -0,0 +1,68 @@
+package frc.robot.subsystems;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+import java.time.Instant;
+
+import com.revrobotics.spark.SparkBase;
+import frc.robot.statemachine.StateBasedSubsystem;
+import frc.robot.enums.RollerState;
+public class KitbotRoller extends StateBasedSubsystem<RollerState>{
+
+
+
+private SparkMax _motor;
+
+    public KitbotRoller (){
+        _motor = new SparkMax(14, MotorType.kBrushed);
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.inverted(false)
+            .idleMode(IdleMode.kCoast);
+
+        _motor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+        _currentState = RollerState.STOP;
+        _previousState = RollerState.ROLL;
+
+    }
+    public Command stop(){
+        return run(() -> _motor.set(0));
+
+
+    }
+    public Command roll(){
+        return run(() -> _motor.set(0.5));
+    }
+    public Command handleCurrentState(){
+        switch (_currentState){
+            case STOP:
+            return stop();
+
+            case ROLL:
+            return roll();
+
+            default:
+            return stop();
+        }
+    }
+    @Override
+    public InstantCommand setWantedState(RollerState state){
+        return new InstantCommand(() -> {
+            if (state != _currentState){
+                _previousState = _currentState;
+                _currentState = state;
+            }
+        }, this);
+    
+    }
+    @Override
+    public void periodic(){
+        handleCurrentState().schedule();
+    }
+}
