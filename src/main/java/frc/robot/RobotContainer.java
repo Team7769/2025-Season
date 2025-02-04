67db74bf9b786002d1aade735e963fa3de43c5bd
SCPhillips06 <128173664+SCPhillips06@users.noreply.github.com>
2025-02-03 19:53:36 -0500

    Feature/32 create the claw subsystem (#36)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.enums.CageState;
+import frc.robot.enums.ClawState;
 import frc.robot.enums.DrivetrainState;
 import frc.robot.enums.LocationTarget;
 import frc.robot.subsystems.Drivetrain;
@@ -25,6 +26,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.enums.RollerState;
 import frc.robot.subsystems.Ascendinator;
 import frc.robot.subsystems.KitbotRoller;
+import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.Vision;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -43,12 +45,14 @@ public class RobotContainer {
   private final CommandXboxController _driverController;
   private final CommandXboxController _operatorController;
   private final Drivetrain _drivetrain;
+  private final Claw _claw;
   private final Ascendinator _ascendinator;
   private final KitbotRoller _roller;
   private final Vision _vision;
   private final SendableChooser<Command> _autoChooser;
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    _claw = new Claw();
     _ascendinator = new Ascendinator();
     _roller = new KitbotRoller();
     _driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
@@ -80,25 +84,33 @@ public class RobotContainer {
     ));
 
     
-    _driverController.rightTrigger().onTrue(_roller.setWantedState(RollerState.ROLL)).onFalse(_roller.setWantedState(RollerState.STOP));
+    //_driverController.rightTrigger().onTrue(_roller.setWantedState(RollerState.ROLL)).onFalse(_roller.setWantedState(RollerState.STOP));
+    _driverController.rightTrigger().onTrue(_claw.setWantedState(ClawState.SCORE)).onFalse(_claw.setWantedState(ClawState.IDLE));
     _driverController.start().onTrue(_drivetrain.resetGyro());
     _driverController.leftBumper().onTrue(_drivetrain.setWantedState(DrivetrainState.TARGET_FOLLOW))
     .onFalse(_drivetrain.setWantedState(DrivetrainState.OPEN_LOOP));
+
     new Trigger (_ascendinator::hasCage).negate().and(_driverController.back()).onTrue(_ascendinator.setWantedState(CageState.DEPLOY));
     new Trigger(_ascendinator::hasCage).and(_driverController.back()).onTrue(_ascendinator.setWantedState(CageState.ASCEND));
+
+    new Trigger(_claw::hasAlgae).negate().and(_driverController.rightBumper()).onTrue(_claw.setWantedState(ClawState.FLOOR_INTAKE)).onFalse(_claw.setWantedState(ClawState.IDLE));
     
     // new Trigger(DriverStation::isAutonomousEnabled).onTrue(_drivetrain.setWantedState(DrivetrainState.AUTO));
     new Trigger(DriverStation::isTeleopEnabled).onTrue(_drivetrain.setWantedState(DrivetrainState.OPEN_LOOP));
 
-    _operatorController.povRight().onTrue(new InstantCommand(()-> _drivetrain.setReefTargetSideRight(0)));
-    _operatorController.povUp().onTrue(new InstantCommand(()-> _drivetrain.setReefTargetSideRight(1)));
-    _operatorController.povLeft().onTrue(new InstantCommand(()-> _drivetrain.setReefTargetSideRight(2)));
-    _operatorController.x().onTrue(new InstantCommand(() -> _drivetrain.targetNextReefFace()));
-    _operatorController.b().onTrue(_drivetrain.setWantedTarget(LocationTarget.REEF));
-    _operatorController.y().onTrue(_drivetrain.setWantedTarget(LocationTarget.CORAL_SOURCE));
-    _operatorController.back().onTrue(_drivetrain.setWantedTarget(LocationTarget.CAGE));
-    _operatorController.a().onTrue(_drivetrain.setWantedTarget(LocationTarget.PROCESSOR));
-    _operatorController.rightBumper().onTrue(_drivetrain.setWantedTarget(LocationTarget.BARGE));
+    _operatorController.a().onTrue(_claw.setWantedState(ClawState.DEALGIFY)).onFalse(_claw.setWantedState(ClawState.IDLE));
+    _operatorController.y().onTrue(_claw.setWantedState(ClawState.PREP_NET));
+    _operatorController.x().onTrue(_claw.setWantedState(ClawState.PREP_PROCESSOR));
+
+    // _operatorController.povRight().onTrue(new InstantCommand(()-> _drivetrain.setReefTargetSideRight(0)));
+    // _operatorController.povUp().onTrue(new InstantCommand(()-> _drivetrain.setReefTargetSideRight(1)));
+    // _operatorController.povLeft().onTrue(new InstantCommand(()-> _drivetrain.setReefTargetSideRight(2)));
+    // _operatorController.x().onTrue(new InstantCommand(() -> _drivetrain.targetNextReefFace()));
+    // _operatorController.b().onTrue(_drivetrain.setWantedTarget(LocationTarget.REEF));
+    // _operatorController.y().onTrue(_drivetrain.setWantedTarget(LocationTarget.CORAL_SOURCE));
+    // _operatorController.back().onTrue(_drivetrain.setWantedTarget(LocationTarget.CAGE));
+    // _operatorController.a().onTrue(_drivetrain.setWantedTarget(LocationTarget.PROCESSOR));
+    // _operatorController.rightBumper().onTrue(_drivetrain.setWantedTarget(LocationTarget.BARGE));
   }
 
   /**
