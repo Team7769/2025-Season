9b5cbf96c3850eb5d0188b83650a69c7fd227e8c
LamaOstrich <kaijvw@gmail.com>
2025-02-05 18:32:13 -0500

    little edits

--- src/main/java/frc/robot/subsystems/Elevatinator.java
+++ src/main/java/frc/robot/subsystems/Elevatinator.java
@@ -7,7 +7,6 @@ import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -21,7 +20,6 @@ public class Elevatinator extends StateBasedSubsystem<ElavatinatorState>{
     private TalonFX _liftMotorinator;
     private Slot0Configs PIDConfiginator;
     private MotionMagicVoltage requestinator;
-    private VoltageOut voltageRequestinator;
     private double manualPositioninator;
     private MotionMagicConfigs motionMagicConfiginator;
     private TalonFXConfiguration talonFXConfiginator;
@@ -81,6 +79,6 @@ public class Elevatinator extends StateBasedSubsystem<ElavatinatorState>{
     private void sysidSetup() {
         elevatorRoutine = new SysIdRoutine(new SysIdRoutine.Config(null, Volts.of(4), null,
             state -> SignalLogger.writeString("SysidElevatinator_State", state.toString())), 
-        new Mechanism(output -> _liftMotorinator.setControl(voltageRequestinator.withOutput(output)), null, this));
+        new Mechanism(output -> _liftMotorinator.setVoltage(output.magnitude()), null, this));
     }
 }
